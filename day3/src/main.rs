26f8a1b20a48ed8b8d72e86e4fa4d0802fdb082d
k.hamada <k.hamada.1988@gmail.com>
2018-12-22 15:16:26 +0900

    [day3] Sloved part2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,11 +1,35 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader, Result};
-#[macro_use] extern crate lazy_static;
-extern crate regex;
+use lazy_static::lazy_static;
 use regex::Regex;
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 
 fn main() -> Result<()> {
+    slove2()
+}
+
+fn slove2() -> Result<()> {
+    let claims = BufReader::new(File::open("input.txt")?).lines()
+        .filter_map(|line| line.ok().map(|input| Claim::new(input)))
+        .collect::<Vec<_>>();
+
+    let mut points = HashMap::new();
+    for claim in claims.iter() {
+        for point in claim.points() {
+            points.entry(point).or_insert_with(Vec::new).push(claim.id);
+        }
+    }
+
+    let claim_ids = claims.iter().map(|claim| &claim.id).collect::<HashSet<_>>();
+    let dup_ids = points.iter().filter(|(_, ids)| ids.len() > 1).flat_map(|(_, ids)| ids).collect::<HashSet<_>>();
+    for single_id in claim_ids.difference(&dup_ids) {
+        println!("{:?}", single_id);
+    }
+
+    Ok(())
+}
+
+fn slove1() -> Result<()> {
     let inputs = BufReader::new(File::open("input.txt")?).lines().filter_map(|line| line.ok());
     let mut points = HashSet::new();
     let mut overlap = HashSet::new();
@@ -22,7 +46,14 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-#[derive(Debug)]
+#[derive(Clone, Debug, PartialEq)]
+struct Point {
+    x: usize,
+    y: usize,
+    id: usize,
+}
+
+#[derive(Clone, Debug)]
 struct Claim {
     id:    usize,
     left:  usize,
